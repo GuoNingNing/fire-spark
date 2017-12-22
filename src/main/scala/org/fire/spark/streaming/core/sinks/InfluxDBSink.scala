645855c3ff90635e4028d7e65a97bf31c22c73ae
zhaozhen01 <zhaozhen01@zuoyebang.com>
2017-12-22 18:15:45 +0800

    添加redisSink和InfluxDBSink

---
+++ src/main/scala/org/fire/spark/streaming/core/sinks/InfluxDBSink.scala
@@ -0,0 +1,49 @@
+package org.fire.spark.streaming.core.sinks
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.{StreamingContext, Time}
+import scalaj.http._
+
+import scala.reflect.ClassTag
+
+/**
+  * Created by cloud on 17/12/22.
+  *
+  * 暂不支持checkpoint模式
+  */
+class InfluxDBSink[T : ClassTag](@transient
+                                 override val ssc : StreamingContext)
+  extends Sink[T]{
+
+  private val host = sparkConf.get("spark.sink.influxDB.host")
+  private val port = sparkConf.get("spark.sink.influxDB.port","8086")
+  private val db = sparkConf.get("spark.sink.influxDB.db","influx")
+
+  def output(rdd : RDD[T],time : Time = Time(System.currentTimeMillis())): Unit = {
+    rdd.foreach(d => {
+      val (postData,ip,pt,dbName) = d match {
+        case t : String => (t,host,port,db)
+        case (k : String,v : String) =>
+          val info = k.split(",")
+          info.size match {
+            case 1 => (v,info(0),port,db)
+            case 2 => (v,info(0),info(1),db)
+            case 3 => (v,info(0),info(1),info(2))
+            case _ => (v,host,port,db)
+          }
+        case (h : String,p : String,d : String,v : String) => (v,h,p,d)
+        case _ => (d.toString,host,port,db)
+      }
+      val res: HttpResponse[String] = Http(s"http://$ip:$pt/write?db=$dbName").postData(postData).asString
+      res.code match {
+        case d if d >= 200 && d < 300 =>
+          logger.info(s"Write influxDB successful. ${res.code}")
+        case d if d >= 400 =>
+          logger.error(s"Write influxDB failed. $res")
+        case _ =>
+          logger.warn(s"Write influxDB unknown error. $res")
+      }
+    })
+  }
+
+}
--- src/main/scala/org/fire/spark/streaming/core/sinks/RedisSink.scala
+++ src/main/scala/org/fire/spark/streaming/core/sinks/RedisSink.scala
@@ -3,28 +3,41 @@ package org.fire.spark.streaming.core.sinks
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.{StreamingContext, Time}
 import org.fire.spark.streaming.core.plugins.redis.{RedisConnectionPool, RedisEndpoint}
+import redis.clients.jedis.Protocol
 
 import scala.reflect.ClassTag
 import scala.reflect.runtime.universe.TypeTag
 
 /**
   * Created by cloud on 17/12/14.
+  *
+  * 暂不支持checkpoint模式
   */
 class RedisSink[T <: scala.Product : ClassTag : TypeTag](@transient
                                                          override
                                                          val ssc : StreamingContext)
   extends Sink[T]{
+  private lazy val host = sparkConf.get("spark.sink.redis.host",Protocol.DEFAULT_HOST)
+  private lazy val port = sparkConf.getInt("spark.sink.redis.port",Protocol.DEFAULT_PORT)
+  private lazy val auth = sparkConf.get("spark.sink.redis.auth",null)
+  private lazy val db = sparkConf.getInt("spark.sink.redis.db",Protocol.DEFAULT_DATABASE)
+  private lazy val timeout = sparkConf.getInt("spark.sink.redis.timeout",Protocol.DEFAULT_TIMEOUT)
 
-  private val redisEndpoint = new RedisEndpoint(sparkConf)
+
+  private val redisEndpoint = RedisEndpoint(host,port,auth,db,timeout)
 
   def output(rdd : RDD[T], time : Time=Time(System.currentTimeMillis())): Unit = {
     rdd.foreachPartition(r => {
       val jedis = RedisConnectionPool.connect(redisEndpoint)
       val pipe = jedis.pipelined()
       r.foreach(d => {
-        val vd = d.asInstanceOf[(Any, Any)]
-        pipe.set(vd._1.toString, vd._2.toString)
-        pipe.expire(vd._1.toString, 3600 * 24 * 7)
+        val (k,v,t) = d match {
+          case n : (String,Any) => (n._1,n._2.toString,3600*24*7)
+          case n : (String,Any,Int) => (n._1,n._2.toString,n._3)
+          case _ => ("UNKNOWN",d.toString,60)
+        }
+        pipe.set(k, v)
+        pipe.expire(k,t)
       })
       pipe.sync()
       pipe.close()
