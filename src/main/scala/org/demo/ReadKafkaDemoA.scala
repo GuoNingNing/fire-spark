b47aabf017e8ff2e4a19aa766b750c4812b1a970
cloudzhao <wlsdhzy@163.com>
2018-04-12 17:55:34 +0800

    add heartbeat

--- src/main/scala/org/demo/ReadKafkaDemoA.scala
+++ src/main/scala/org/demo/ReadKafkaDemoA.scala
@@ -1,27 +1,49 @@
 package org.demo
 
-import kafka.serializer.StringDecoder
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.StreamingContext
-import org.fire.spark.streaming.core.FireStreaming
+import org.fire.spark.streaming.core.kit.Heartbeat
+import org.fire.spark.streaming.core.{FireStreaming, Logging}
 import org.fire.spark.streaming.core.plugins.kafka.KafkaDirectSource
 
-object ReadKafkaDemoA extends FireStreaming {
+object ReadKafkaDemoA extends FireStreaming with Logging {
 
+  private var heartbeat: Heartbeat = _
 
-  /* 初始化，函数，可以设置 sparkConf
+  /**
+    * 初始化，函数，可以设置 sparkConf
     * 初始化自己私有参数的方法,非必需的,看个人需求
     * @param sparkConf
     */
   override def init(sparkConf: SparkConf): Unit = {
-    sparkConf.set("spark.xxx","x")
+    logInfo("init SparkConf")
+    sparkConf.set("spark.xxx", "x")
+
+    /**
+      * 用于添加在sparkContext启动时注册能接收所有事件的SparkListener
+      * 建议直接在配置文件中添加
+      */
+    addSparkListeners("org.apache.spark.StartSparkAppListener")
   }
 
-  /*
-  * 用于添加在sparkContext启动时注册能接收所有事件的listener
-  * 非必需的,看需要使用
-  * */
-  addAllEventListeners("org.apache.spark.StartSparkAppListener")
+
+  /**
+    * StreamingContext 运行之后执行
+    */
+  override def afterStarted(ssc: StreamingContext): Unit = {
+    logInfo("StreamingContext already start.")
+    heartbeat = new Heartbeat(ssc)
+    heartbeat.start()
+  }
+
+  /**
+    *  StreamingContext 停止后 程序停止前 执行
+    */
+  override def beforeStop(ssc: StreamingContext): Unit = {
+    if(heartbeat != null){
+      heartbeat.stop()
+    }
+  }
 
   /**
     * 处理函数
@@ -38,7 +60,5 @@ object ReadKafkaDemoA extends FireStreaming {
       rdd.take(10).foreach(println)
       source.updateOffsets(time.milliseconds)
     })
-
-
   }
 }
---
+++ src/main/scala/org/fire/spark/streaming/core/FireSpark.scala
@@ -0,0 +1,82 @@
+package org.fire.spark.streaming.core
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.fire.spark.streaming.core.kit.Utils
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * Created by cloud on 18/4/12.
+  */
+trait FireSpark {
+
+  protected final def args: Array[String] = _args
+
+  private final var _args: Array[String] = _
+
+  private val sparkListeners = new ArrayBuffer[String]()
+
+  /**
+    * 初始化，函数，可以设置 sparkConf
+    *
+    * @param sparkConf
+    */
+  def init(sparkConf: SparkConf): Unit = {}
+
+  /**
+    * spark 启动后 调用
+    */
+  def afterStarted(sc: SparkContext): Unit = {}
+
+  /**
+    * spark 停止后 程序结束前 调用
+    */
+  def beforeStop(sc: SparkContext): Unit = {}
+
+  /**
+    * 添加sparkListener
+    * 如使用此函数添加,则必须在 init 函数中调用此函数
+    * @param listener
+    * @deprecated 建议直接在配置文件中添加
+    */
+  @deprecated
+  def addSparkListeners(listener : String): Unit = sparkListeners += listener
+
+  /**
+    * 处理函数
+    *
+    * @param sc
+    */
+  def handle(sc: SparkContext): Unit
+
+  def creatingContext(): SparkContext = {
+    val sparkConf = new SparkConf()
+
+    // 约定传入此参数,则表示本地 Debug
+    if (sparkConf.contains("spark.properties.file")) {
+      sparkConf.setAll(Utils.getPropertiesFromFile(sparkConf.get("spark.properties.file")))
+      sparkConf.setAppName("LocalDebug").setMaster("local[*]")
+      sparkConf.set("spark.streaming.kafka.maxRatePerPartition", "10")
+    }
+
+    init(sparkConf)
+
+    val extraListeners = sparkListeners.mkString(",") + "," + sparkConf.get("spark.extraListeners","")
+    if (extraListeners != "") sparkConf.set("spark.extraListeners",extraListeners)
+
+    val sc = new SparkContext(sparkConf)
+    handle(sc)
+    sc
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    this._args = args
+
+    val context = creatingContext()
+    afterStarted(context)
+    context.stop()
+    beforeStop(context)
+  }
+
+}
