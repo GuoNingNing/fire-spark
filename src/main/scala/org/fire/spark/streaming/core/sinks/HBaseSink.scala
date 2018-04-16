5b3939c10d676cd398af2fec76cf837404c4ea80
cloudzhao <wlsdhzy@163.com>
2018-04-16 18:20:28 +0800

    delete HBaseBulkSink

--- src/main/scala/org/fire/spark/streaming/core/sinks/HBaseSink.scala
+++ src/main/scala/org/fire/spark/streaming/core/sinks/HBaseSink.scala
@@ -1,5 +1,6 @@
 package org.fire.spark.streaming.core.sinks
 
+import java.util.{ArrayList => JAList}
 import java.util.Properties
 
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
@@ -11,7 +12,6 @@ import org.fire.spark.streaming.core.plugins.hbase.HbaseConnPool
 
 import scala.reflect.ClassTag
 import scala.collection.JavaConversions._
-import scala.collection.mutable.ArrayBuffer
 
 /**
   * Created by cloud on 18/4/3.
@@ -31,15 +31,25 @@ class HBaseSink[T <: Mutation : ClassTag](@transient override val sc: SparkConte
   private val tableName = prop.getProperty("hbase.table")
   private val commitBatch = prop.getProperty("hbase.commit.batch","1000").toInt
   private val bufferSize = prop.getProperty("hbase.commit.buffer",s"${5*1024*1024}").toLong
+  private val maxValueSize = prop.getProperty("hbase.max.value","1000").toInt
 
-  private def getTable : BufferedMutator = {
+  private def getConnect: Connection = {
     val conf = HBaseConfiguration.create
     prop.foreach {case (k,v) => conf.set(k,v)}
-    val connection = HbaseConnPool.connect(conf)
+    HbaseConnPool.connect(conf)
+  }
+
+  private def getMutator: BufferedMutator = {
+    val connection = getConnect
     val bufferedMutatorParams = new BufferedMutatorParams(TableName.valueOf(tableName))
     bufferedMutatorParams.writeBufferSize(bufferSize)
-    bufferedMutatorParams.maxKeyValueSize(commitBatch)
+    bufferedMutatorParams.maxKeyValueSize(maxValueSize)
     connection.getBufferedMutator(bufferedMutatorParams)
+  }
+
+  private def getTable: Table = {
+    val connection = getConnect
+    connection.getTable(TableName.valueOf(tableName))
   }
 
   /** 输出
@@ -48,16 +58,30 @@ class HBaseSink[T <: Mutation : ClassTag](@transient override val sc: SparkConte
     * @param time
     */
   override def output(rdd: RDD[T], time: Time = Time(System.currentTimeMillis())): Unit = {
-    rdd.foreachPartition { r =>
-      val table = getTable
-
-      r.foreach {
-        case put: Put => table.mutate(put)
-        case del: Delete => table.mutate(del)
+    rdd match {
+      case r: RDD[Put] => r.foreachPartition { putRDD =>
+        val mutator = getMutator
+        putRDD.foreach { p => mutator.mutate(p.asInstanceOf[Put]) }
+        mutator.flush()
+        mutator.close()
       }
 
-      table.flush()
-      table.close()
+      case r: RDD[Delete] => r.foreachPartition { delRDD =>
+        val table = getTable
+        val delList = new JAList[Delete]()
+        delRDD.foreach { d =>
+          delList += d.asInstanceOf[Delete]
+          if(delList.size() >= commitBatch){
+            table.batch(delList,null)
+            delList.clear()
+          }
+        }
+        if(delList.size() > 0){
+          table.batch(delList,null)
+          delList.clear()
+        }
+        table.close()
+      }
     }
   }
 
