8306555354f1be309f8fcdc0fb6a555ca18a69c2
虚怀 <xuhuai@ibantang.com>
2018-05-31 19:52:24 +0800

    添加 读取阿里云MNS （残疾版）

--- src/main/scala/org/fire/spark/streaming/core/plugins/aliymns/AliyMNSSource.scala
+++ src/main/scala/org/fire/spark/streaming/core/plugins/aliymns/AliyMNSSource.scala
@@ -1,6 +1,5 @@
 package org.fire.spark.streaming.core.plugins.aliymns
 
-import com.aliyun.mns.client.{CloudAccount, CloudQueue, MNSClient}
 import com.aliyun.mns.model.Message
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.DStream
@@ -8,8 +7,23 @@ import org.fire.spark.streaming.core.plugins.aliymns.manager.MNSManager
 import org.fire.spark.streaming.core.sources.Source
 
 import scala.reflect.ClassTag
-import scala.collection.JavaConverters._
 
+/**
+  * <pre>
+  *
+  * spark.source.aliymns.consume.access.id=
+  * spark.source.aliymns.consume.access.key=
+  * spark.source.aliymns.consume.endpoint=
+  * spark.source.aliymns.consume.queue.name=
+  * spark.source.aliymns.consume.batchsize=
+  * spark.source.aliymns.consume.waittime=
+  * spark.source.aliymns.consume.taskNum=
+  *
+  * </pre>
+  *
+  * @param ssc
+  * @param specialParams
+  */
 class AliyMNSSource(@transient val ssc: StreamingContext,
                     specialParams: Map[String, String] = Map.empty[String, String]) extends Source {
   override val paramPrefix: String = "spark.source.aliymns.consume."
