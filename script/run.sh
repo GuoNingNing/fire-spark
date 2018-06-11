3c0cf7aa65774b46a325d929d4a59a08e06c849a
cloudzhao <wlsdhzy@163.com>
2018-06-11 16:01:04 +0800

    调整;;bug

--- script/run.sh
+++ script/run.sh
@@ -90,11 +90,11 @@ function set_default_lib(){
 	local p=$(basename $1)
 
 	if [ $(echo $1 | grep -E "conf/online/$p$" | wc -l) -ne 0 ];then
-		echo "../../lib";;
+		echo "../../lib"
 	elif [ $(echo $1 | grep -E "conf/$p$" | wc -l) -ne 0 ];then
 		echo "../lib"
 	else
-		echo "lib";;
+		echo "lib"
 	fi
 }
 
