0d6c8594b8d47f5d28776bc9911bdf89c8cb227a
shumeng.ren <shumeng.ren@meelive.cn>
2018-07-14 19:12:52 +0800

    [update] 1.增加邮件报警

--- src/main/scala/notice.scala
+++ src/main/scala/notice.scala
@@ -1,3 +1,9 @@
+import javax.mail.internet.InternetAddress
+import javax.mail.{Authenticator, PasswordAuthentication}
+
+import com.solarmosaic.client.mail.EnvelopeWrappers
+import com.solarmosaic.client.mail.content.ContentType.MultipartTypes
+
 /**
   * Created by guoning on 2017/6/6.
   *
@@ -10,8 +16,21 @@ package object notice {
   import org.fire.spark.streaming.core.kit.Utils
 
   case class Ding(api: String, to: String, message: String)
+  case class EMail(to:String,
+                   subject:String,
+                   message:String,
+                   user:String,
+                   password:String,
+                   addr:String="",
+                   port:Int = 587,
+                   tls:Boolean=false
+                  ) extends Authenticator {
+    override def getPasswordAuthentication(): PasswordAuthentication = {
+      new PasswordAuthentication(user, password)
+    }
+  }
 
-  object send {
+  object send extends EnvelopeWrappers {
 
     def a(ding: Ding): Unit = {
 
@@ -35,6 +54,35 @@ package object notice {
       val (code,res) = Utils.httpPost(ding.api,body,headers)
 
       println(s"result code : $code , body : $res")
+    }
+
+
+    def a(email: EMail): Unit = {
+
+      import com.solarmosaic.client.mail._
+      import com.solarmosaic.client.mail.configuration._
+      import com.solarmosaic.client.mail.content._
+
+
+      val config = SmtpConfiguration(host = email.addr,
+                                      port = email.port,
+                                      tls = email.tls,
+                                      debug = true,
+                                      authenticator = Some(email)
+                 )
+      val mailer = Mailer(config)
+      val content = Multipart(
+        parts = Seq(Text("text"), Html(s"<p>${email.message}</p>")),
+        subType = MultipartTypes.alternative
+      )
+
+      val envelope = Envelope(
+        from = email.user,
+        to = Seq(email.to),
+        subject = email.subject,
+        content = content
+      )
+      mailer.send(envelope)
     }
   }
 
