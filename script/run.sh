a351429dfbc5065ba5594fad96ef0c1309b9df12
cloudzhao <wlsdhzy@163.com>
2018-09-11 16:15:12 +0800

    调整退出状态码

--- script/run.sh
+++ script/run.sh
@@ -28,7 +28,7 @@ function check_env(){
 		else
 			test "x${!var}" == "x" && "x$d" == "x" && {
 				echo "env variable $var not set." >&2;
-				exit;
+				exit 2;
 			}
         fi
 }
@@ -37,15 +37,15 @@ function get_param(){
         local n=$2
         local d=$3
 
-	test ! -f "$user_proper_file" && { echo "Properties $user_proper_file file not set">&2;exit; }
+	test ! -f "$user_proper_file" && { echo "Properties $user_proper_file file not set">&2;exit 2; }
         local v=$(grep "^$n=" $user_proper_file | head -1 | awk -F '=' '{s="";for(i=2;i<=NF;i++){if(s){s=s"="$i}else{s=$i}};print s}')
         test "x$v" == "x" && test "x$d" != "x" && v="$d"
-        test "x$v" == "x" && { echo "$n not set">&2;exit; }
+        test "x$v" == "x" && { echo "$n not set">&2;exit 2; }
         eval "$var=\"$v\""
 }
 function set_jars(){
 	local jar=""
-	test ! -d "$lib_path" && { echo "lib_path $lib_path not found.">&2;exit; }
+	test ! -d "$lib_path" && { echo "lib_path $lib_path not found.">&2;exit 2; }
 	for jar in $(ls $lib_path)
 	do
 	    if [ "x$jar" != "x$(basename $main_jar)" ]; then
@@ -58,7 +58,7 @@ function check_command(){
 	local c=${1:-"yarn"}
 	if ! which $c >/dev/null 2>&1;then
 		echo "command $c not found.">&2
-		exit
+		exit 2
 	fi
 }
 function send_ding(){
@@ -78,24 +78,24 @@ function check_run(){
     local flag=$1
     check_command yarn
 	local appids=($(yarn application -list | awk -v app=$appname '{if($2==app){print $1}}'))
-    test ${#appids[@]} -eq 0 && test "x$flag" != "x" && { echo "Spark app $appname already stop.">&2;exit; }
+    test ${#appids[@]} -eq 0 && test "x$flag" == "xstop" && { echo "Spark app $appname already stop.">&2;exit 2; }
     if test ${#appids[@]} -ne 0;then
-  	if [ "x$flag" != "x" ];then
+  	if [ "x$flag" == "xstop" ];then
 		for flag in ${appids[@]}
 		do
 			echo "yarn application -kill $flag">&2
 			yarn application -kill $flag
 		done
-		exit;
+		exit 0;
 	fi
         echo "Spark app $appname already running. ${appids[@]}" >&2;
-        exit;
+        exit 2;
     fi
 }
 function set_abs_lib(){
         local p=$1
         local lp=$2
-        test $(echo $p | grep -E '\.properties$' | wc -l) -eq 0 && { echo "$p the file needs to be of type properties">&2;exit; }
+        test $(echo $p | grep -E '\.properties$' | wc -l) -eq 0 && { echo "$p the file needs to be of type properties">&2;exit 2; }
         local cap=$(cd $(dirname $p);pwd)
 		test "x${!lp:0:1}" != "x/" && eval "$lp=$(get_abs_path $cap/${!lp})"
 }
@@ -146,7 +146,7 @@ function clear_once(){
 function main(){
 	local proper=${1:-"$(basename $base).properties"};shift
 	proper=$(set_conf_dir $proper)
-	test ! -f "$proper" && { echo "file $proper not found">&2;exit; }
+	test ! -f "$proper" && { echo "file $proper not found">&2;exit 2; }
 	proper=$(get_abs_path $proper)
 	user_proper_file=$proper
 
@@ -169,7 +169,7 @@ function main(){
 
 	set_abs_lib "$proper" "lib_path"
 	main_jar=$lib_path/$main_jar
-	test ! -f "$main_jar" && { echo "$main_jar file does not exist">&2;exit; }
+	test ! -f "$main_jar" && { echo "$main_jar file does not exist">&2;exit 2; }
 	#test "x$self_param" != "x" && self_params=($(echo $self_param | awk -F ',' '{for(i=1;i<=NF;i++){print $i}}'))
 
 	local is_stop=""
@@ -189,6 +189,6 @@ function main(){
 
 test $# -eq 0 && { 
 	echo -e "Usage Ex:\n\tbash $base/$0 kafka_2_hdfs.properties">&2;
-	exit;
+	exit 2;
 }
 main "$@"
