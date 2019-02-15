0ea900648c6d5e273625bce42f673cade072b3ed
shumeng.ren <shumeng.ren@meelive.cn>
2019-02-15 16:42:42 +0800

    [update] 1.更新运行脚本

--- script/run.sh
+++ script/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 base=$(cd $(dirname $0);pwd)
 
+export SPARK_KAFKA_VERSION=0.10
+
 function my_include(){
     local f=${1:-"$HOME/.bash_profile"}
     test -f $f && . $f
@@ -28,7 +30,7 @@ function check_env(){
 		else
 			test "x${!var}" == "x" && "x$d" == "x" && {
 				echo "env variable $var not set." >&2;
-				exit;
+				exit 2;
 			}
         fi
 }
@@ -37,15 +39,15 @@ function get_param(){
         local n=$2
         local d=$3
 
-	test ! -f "$user_proper_file" && { echo "Properties $user_proper_file file not set">&2;exit; }
+	test ! -f "$user_proper_file" && { echo "Properties $user_proper_file file not set">&2;exit 2; }
         local v=$(grep "^$n=" $user_proper_file | head -1 | awk -F '=' '{s="";for(i=2;i<=NF;i++){if(s){s=s"="$i}else{s=$i}};print s}')
         test "x$v" == "x" && test "x$d" != "x" && v="$d"
-        test "x$v" == "x" && { echo "$n not set">&2;exit; }
+        test "x$v" == "x" && { echo "$n not set">&2;exit 2; }
         eval "$var=\"$v\""
 }
 function set_jars(){
 	local jar=""
-	test ! -d "$lib_path" && { echo "lib_path $lib_path not found.">&2;exit; }
+	test ! -d "$lib_path" && { echo "lib_path $lib_path not found.">&2;exit 2; }
 	for jar in $(ls $lib_path)
 	do
 	    if [ "x$jar" != "x$(basename $main_jar)" ]; then
@@ -58,9 +60,10 @@ function check_command(){
 	local c=${1:-"yarn"}
 	if ! which $c >/dev/null 2>&1;then
 		echo "command $c not found.">&2
-		exit
+		exit 2
 	fi
 }
+
 function send_ding(){
 	local token=$1
 	local contacts=$2
@@ -74,35 +77,44 @@ function send_ding(){
 	curl -s ${url:-"https://oapi.dingtalk.com/robot/send?access_token=$token"} -H 'Content-Type: application/json' -d "$data"
 	echo
 }
-function check_run(){
+
+function check_run() {
     local flag=$1
     check_command yarn
 	local appids=($(yarn application -list | awk -v app=$appname '{if($2==app){print $1}}'))
-    test ${#appids[@]} -eq 0 && test "x$flag" != "x" && { echo "Spark app $appname already stop.">&2;exit; }
+    test ${#appids[@]} -eq 0 && test "x$flag" == "xstop" && { echo "Spark app $appname already stop.">&2;exit 2; }
     if test ${#appids[@]} -ne 0;then
-  	if [ "x$flag" != "x" ];then
-		for flag in ${appids[@]}
+		local appid=${appids[0]}
+		local id=""
+		unset appids[0]
+		for id in ${appids[@]}
 		do
-			echo "yarn application -kill $flag">&2
-			yarn application -kill $flag
+			echo "yarn application -kill $id">&2
+			yarn application -kill $id
 		done
-		exit;
-	fi
-        echo "Spark app $appname already running. ${appids[@]}" >&2;
-        exit;
+		if [ "x$flag" == "xstop" ];then
+			echo "yarn application -kill $appid">&2
+			yarn application -kill $appid
+			exit 0;
+		fi
+        echo `date `" Spark app $appname already running. $appid" >&2;
+        exit 2;
     fi
 }
+
 function set_abs_lib(){
         local p=$1
         local lp=$2
-        test $(echo $p | grep -E '\.properties$' | wc -l) -eq 0 && { echo "$p the file needs to be of type properties">&2;exit; }
+        test $(echo $p | grep -E '\.properties$' | wc -l) -eq 0 && { echo "$p the file needs to be of type properties">&2;exit 2; }
         local cap=$(cd $(dirname $p);pwd)
 		test "x${!lp:0:1}" != "x/" && eval "$lp=$(get_abs_path $cap/${!lp})"
 }
 function set_default_lib(){
 	local p=$(basename $1)
 
-	if [ $(echo $1 | grep -E "conf/online/$p$" | wc -l) -ne 0 ];then
+	if [ $(echo $1 | grep -E "common/conf/online/$p$" | wc -l) -ne 0 ];then
+		echo "../../../lib"
+	elif [ $(echo $1 | grep -E "conf/online/$p$" | wc -l) -ne 0 ];then
 		echo "../../lib"
 	elif [ $(echo $1 | grep -E "conf/$p$" | wc -l) -ne 0 ];then
 		echo "../lib"
@@ -116,52 +128,91 @@ function set_conf_dir(){
 		echo "conf/$1"
 	elif [ -f "conf/online/$1" ];then
 		echo "conf/online/$1"
+    elif [ -f "common/conf/online/$1" ];then
+		echo "common/conf/online/$1"
 	else
 		echo "$1"
 	fi
 }
 
+function check_once(){
+	local app_name=$1
+	local pid=$$
+	if test -f $app_name && kill -0 $(cat $app_name) >/dev/null 2>&1;then
+		echo "run.sh already running. pid is $(cat $app_name)" >&2
+		exit 2;
+	fi
+	echo $pid >$app_name
+}
+
+function clear_once(){
+	local app_name=$1
+	local pid=$$
+	while :
+	do
+		if ! kill -0 $pid >/dev/null 2>&1;then
+			rm -rf $app_name
+		fi
+		sleep 1
+	done
+}
+
 function main(){
-	local proper=${1:-"$(basename $base).properties"}
+	local proper=${1:-"$(basename $base).properties"};shift
 	proper=$(set_conf_dir $proper)
-	test ! -f "$proper" && { echo "file $proper not found">&2;exit; }
+	test ! -f "$proper" && { echo "file $proper not found">&2;exit 2; }
 	proper=$(get_abs_path $proper)
 	user_proper_file=$proper
 
 	my_include
+	my_include /etc/profile
+	my_include /etc/bashrc
+	my_include $HOME/.bashrc
 	test $(check_cmd "java") -eq 0 && check_env "JAVA_HOME"
 	test $(check_cmd "spark-submit") -eq 0 && check_env "SPARK_HOME"
 
 	get_param "main" "spark.run.main"
 	get_param "main_jar" "spark.run.main.jar"
 	get_param "appname" "spark.app.name" "${main}.App"
-	get_param "self_param" "spark.run.self.params" "#"
+	#get_param "self_param" "spark.run.self.params" "#"
 	get_param "lib_path" "spark.run.lib.path" $(set_default_lib $proper)
 
 	get_param "ding_token" "spark.run.alert.ding.api" "#"
 	get_param "ding_contacts" "spark.run.alert.ding.contacts" "#"
 	get_param "ding_context" "spark.run.alert.ding.context" "任务已经开始提交"
 
+    # 离线任务将日期加上
+    if [[ "$1" =~ ^([0-9]{8})$ ]];then
+        appname=$appname${1}
+    fi
+
+	#check_once "$(dirname $proper)/.${appname}"
+	#clear_once "$(dirname $proper)/.${appname}" &
+
 	set_abs_lib "$proper" "lib_path"
 	main_jar=$lib_path/$main_jar
-	test ! -f "$main_jar" && { echo "$main_jar file does not exist">&2;exit; }
-	test "x$self_param" != "x#" && self_params=($(echo $self_param | awk -F ',' '{for(i=1;i<=NF;i++){print $i}}'))
+	test ! -f "$main_jar" && { echo "$main_jar file does not exist">&2;exit 2; }
+	#test "x$self_param" != "x" && self_params=($(echo $self_param | awk -F ',' '{for(i=1;i<=NF;i++){print $i}}'))
 
-	check_run $2
+	local is_stop=""
+	local last_param=${@: -1}
+	test "x$last_param" == "xstop" && is_stop="stop" && shift
+	check_run $is_stop
 	set_jars
 
 	local spark_submit=spark-submit
-	local main_parameter="--name $appname --properties-file $proper $jars --class $main $main_jar ${self_params[@]}"
-	echo "spark-submit $main_parameter"
+	local main_parameter="--name $appname --properties-file $proper $jars --class $main $main_jar"
+#	echo "spark-submit $main_parameter"
 
 	test $(check_cmd "spark2-submit") -eq 1 && spark_submit=spark2-submit
 	check_command spark-submit
 	send_ding "$ding_token" "$ding_contacts" "$appname $ding_context"
-	spark-submit $main_parameter
+	echo "sudo -u hdfs spark2-submit $main_parameter $@"
+	spark2-submit $main_parameter "$@"
 }
 
-test $# -eq 0 && { 
+test $# -eq 0 && {
 	echo -e "Usage Ex:\n\tbash $base/$0 kafka_2_hdfs.properties">&2;
-	exit;
+	exit 2;
 }
 main "$@"
