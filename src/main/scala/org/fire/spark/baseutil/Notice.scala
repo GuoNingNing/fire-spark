4e705358dbdfd113ecbe043256945e42a3bb34c9
shumeng.ren <shumeng.ren@meelive.cn>
2019-08-28 19:18:59 +0800

    [update] 1.基本信息从0.8合并过来

--- src/main/scala/org/fire/spark/baseutil/Notice.scala
+++ src/main/scala/org/fire/spark/baseutil/Notice.scala
@@ -1,9 +1,11 @@
 package org.fire.spark
 
-import com.solarmosaic.client.mail.EnvelopeWrappers
-import com.solarmosaic.client.mail.content.ContentType.MultipartTypes
 import javax.mail.internet.InternetAddress
 import javax.mail.{Authenticator, PasswordAuthentication}
+
+import com.solarmosaic.client.mail.EnvelopeWrappers
+import com.solarmosaic.client.mail.content.ContentType.MultipartTypes
+import org.apache.spark.SparkConf
 
 /**
   * Created by guoning on 2017/6/6.
@@ -14,77 +16,130 @@ import javax.mail.{Authenticator, PasswordAuthentication}
   */
 package object Notice {
 
-  import org.fire.spark.streaming.core.kit.Utils
+    import org.fire.spark.streaming.core.kit.Utils
 
-  case class Ding(api: String, to: String, message: String)
-  case class EMail(to:String,
-                   subject:String,
-                   message:String,
-                   user:String,
-                   password:String,
-                   addr:String="",
-                   port:Int = 587,
-                   tls:Boolean=false,
-                   subtype:String="text"
-                  ) extends Authenticator {
-    override def getPasswordAuthentication(): PasswordAuthentication = {
-      new PasswordAuthentication(user, password)
+    case class Ding(api: String, to: String, message: String)
+
+    case class EMail(to: String,
+                     subject: String,
+                     message: String,
+                     user: String,
+                     password: String,
+                     addr: String = "",
+                     port: Int = 587,
+                     tls: Boolean = false,
+                     subtype: String = "text"
+                    ) extends Authenticator {
+        override def getPasswordAuthentication(): PasswordAuthentication = {
+            new PasswordAuthentication(user, password)
+        }
     }
-  }
 
-  object send extends EnvelopeWrappers {
+    /**
+      * 发送通知消息
+      *
+      * @param conf
+      * @param info
+      */
+    def sendMessage(conf: SparkConf,
+                    info: Map[String, String] = Map[String, String]()): Unit = {
 
-    def a(ding: Ding): Unit = {
+        val dd_url = info.getOrElse("dd_url", conf.get("spark.dingding.url", ""))
+        val at = info.getOrElse("phone", conf.get("spark.dingding.phone", ""))
 
-      val body =
-        s"""
-           |{
-           |  "msgtype": "text",
-           |  "text": {
-           |    "content": "${ding.message}"
-           |  },
-           |  "at": {
-           |    "atMobiles": [
-           |      ${ding.to}
-           |    ],
-           |    "isAtAll": false
-           |  }
-           |}
+        try {
+
+            val message = info("message").toString
+
+            println(
+                s"""
+                   |dd_url:$dd_url
+                   |at:$at
+                   |message:$message
+                """.stripMargin)
+
+            if (message.length > 0) {
+                send a Ding(dd_url, at, message.replace("</p>", "\n"))
+                send a EMail(
+                    to = conf.get("spark.email.to", ""),
+                    subject = s"${conf.get("spark.email.subject", "离线数据表异常")}",
+                    message = message,
+                    user = conf.get("spark.email.user", ""),
+                    password = conf.get("spark.email.password", ""),
+                    addr = conf.get("spark.email.server", ""),
+                    subtype = "html"
+                )
+            }
+        } catch {
+            case ex: Exception => {
+
+                val errMsg =
+                    s"""
+                       | info:$info
+                       | message:${ex.getMessage}
+                       | stack:${ex.getStackTrace.map(_.toString).mkString("</p>")}
+                    """.stripMargin
+                println(s"send_message exception:${errMsg}")
+                send a Ding(dd_url, at, errMsg.replace("</p>", "\n"))
+            }
+        }
+
+    }
+
+    object send extends EnvelopeWrappers {
+
+        def a(ding: Ding): Unit = {
+
+            val body =
+                s"""
+                   |{
+                   |  "msgtype": "text",
+                   |  "text": {
+                   |    "content": "${ding.message}"
+                   |  },
+                   |  "at": {
+                   |    "atMobiles": [
+                   |      ${ding.to}
+                   |    ],
+                   |    "isAtAll": false
+                   |  }
+                   |}
         """.stripMargin
 
-      val headers = Map("content-type" -> "application/json")
-      val (code, res) = Utils.httpPost(ding.api, body, headers)
-      println(s"result code : $code , body : $res")
+            val headers = Map("content-type" -> "application/json")
+            val (code, res) = Utils.httpPost(ding.api, body, headers)
+
+            println(s"result code : $code , body : $res")
+        }
+
+
+        def a(email: EMail): Unit = {
+
+            import com.solarmosaic.client.mail._
+            import com.solarmosaic.client.mail.configuration._
+            import com.solarmosaic.client.mail.content._
+
+
+            val config = SmtpConfiguration(host = email.addr,
+                port = email.port,
+                tls = email.tls,
+                debug = false,
+                authenticator = Some(email)
+            )
+            val mailer = Mailer(config)
+            val content = Multipart(
+                parts = Seq(Text(email.subtype), Html(s"<p>${email.message}</p>")),
+                subType = MultipartTypes.alternative
+            )
+
+            val envelope = Envelope(
+                from = email.user,
+                to = email.to.split(",").toSeq.map(x => new InternetAddress(x)),
+                subject = email.subject,
+                content = content
+            )
+            mailer.send(envelope)
+        }
     }
-
-
-    def a(email: EMail): Unit = {
-
-      import com.solarmosaic.client.mail._
-      import com.solarmosaic.client.mail.configuration._
-      import com.solarmosaic.client.mail.content._
-
-
-      val config = SmtpConfiguration(host = email.addr,
-                                      port = email.port,
-                                      tls = email.tls,
-                                      debug = false,
-                                      authenticator = Some(email)
-                 )
-      val mailer = Mailer(config)
-      val content = Multipart(
-        parts = Seq(Text(email.subtype), Html(s"<p>${email.message}</p>")),
-        subType = MultipartTypes.alternative
-      )
-
-      val envelope = Envelope(
-        from = new InternetAddress(email.user),
-        to = email.to.split(",").toSeq.map(x => new InternetAddress(x)),
-        subject = email.subject,
-        content = content
-      )
-      mailer.send(envelope)
-    }
-  }
 
 }
--- src/main/scala/org/fire/spark/baseutil/RedisInfo.scala
+++ src/main/scala/org/fire/spark/baseutil/RedisInfo.scala
@@ -2,6 +2,7 @@ package org.fire.spark.baseutil
 
 import net.minidev.json.JSONObject
 import org.fire.spark.streaming.core.plugins.redis.{RedisConnectionPool, RedisEndpoint}
+
 import scala.collection.JavaConversions._
 import scala.collection.mutable
 
@@ -9,7 +10,6 @@ import scala.collection.mutable
   * Created on 2017-10-12.
   * Copyright (c) 2017, ingkee版权所有.
   * Author: shumeng.ren
-  * 将一些公共的操作抽离出来，可能特定的业务会使用到
   */
 object RedisInfo {
 
@@ -17,84 +17,89 @@ object RedisInfo {
     private var timeout = 3600 * 24 * 180
     private var redis_key: String = ""
 
+
     // 信息数据存放map
     private val infoMap = mutable.Map[String, String]()
+    val a = mutable.Map(("1", "1"), ("2", "2"))
 
 
     /**
       * 将关键信息存入
+      *
       * @param key
       * @param value
       * @return
       */
-    def putValue(key:String, value:String): Option[String] = {
+    def putValue(key: String, value: String) = {
         infoMap.put(key, value)
     }
 
     /**
       * 获取map中的数据
+      *
       * @param key
       * @return
       */
-    def getValue(key:String):Option[String] = {
+    def getValue(key: String): Option[String] = {
         infoMap.get(key)
     }
 
     /**
       * 清空map
       */
-    def clear(): Unit = {
+    def clear() = {
         infoMap.clear()
-        redis_key=""
+        redis_key = ""
     }
 
     /**
       * 设置公共前缀
+      *
       * @param prefix
       */
-    def setPrefix(prefix:String): Unit = {
+    def setPrefix(prefix: String) = {
         redis_key = prefix
     }
 
     /**
       * 设置超时时间
+      *
       * @param timeout
       */
-    def setTimeout(timeout:Int): Unit = {
+    def setTimeout(timeout: Int) = {
         this.timeout = timeout
     }
 
 
-    /**
-      * 将数据显示出来
-      */
-    def showData():Unit = {
+    def showData(): Unit = {
         println(s"redis key:$redis_key")
         println(s"redis value:${JSONObject.toJSONString(infoMap)}")
     }
 
     /**
       * 存写数据
+      *
       * @param host
       * @param port
       * @param password
       */
-    def storeData(host:String, port:Int, password:String): Unit = {
+    def storeData(host: String, port: Int, password: String) = {
         val redisPoint = RedisEndpoint(host, port, password)
-        val client = RedisConnectionPool.connect(redisPoint)
+        val pool = RedisConnectionPool.connect(redisPoint)
 
-        println(s"redis_key:$redis_key infomap: $infoMap")
+        println(s"redis_key:$redis_key infomap: ${infoMap}")
         println(s"redis info:$host  $port   $password")
 
         if (!redis_key.isEmpty) {
             val real_value = JSONObject.toJSONString(infoMap)
 
-            client.set(redis_key, real_value)
-            client.expire(redis_key, timeout)
+            pool.set(redis_key, real_value)
+            pool.expire(redis_key, timeout)
 
         } else {
             println("redis_key is empty!!!!")
         }
-        client.close()
+
+        pool.close()
     }
 }
