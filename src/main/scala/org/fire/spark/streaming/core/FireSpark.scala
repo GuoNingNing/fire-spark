9c3f96499812d81148f97d44a7d9affc838dc13c
shumeng.ren <shumeng.ren@meelive.cn>
2019-09-04 09:35:06 +0800

    [update] 1.增加调试信息

--- src/main/scala/org/fire/spark/streaming/core/FireSpark.scala
+++ src/main/scala/org/fire/spark/streaming/core/FireSpark.scala
@@ -12,81 +12,88 @@ import scala.collection.mutable.ArrayBuffer
   */
 trait FireSpark {
 
-  protected final def args: Array[String] = _args
+    protected final def args: Array[String] = _args
 
-  private final var _args: Array[String] = _
+    private final var _args: Array[String] = _
 
-  private val sparkListeners = new ArrayBuffer[String]()
+    // 添加debug 参数，便于统一控制
+    private final var _debug: Boolean = false
 
-  @(transient@getter)
-  var sparkSession: SparkSession = _
+    def isDebug: Boolean = _debug
 
-  /**
-    * 初始化，函数，可以设置 sparkConf
-    *
-    * @param sparkConf
-    */
-  def init(sparkConf: SparkConf): Unit = {}
+    private val sparkListeners = new ArrayBuffer[String]()
 
-  /**
-    * spark 启动后 调用
-    */
-  def afterStarted(sc: SparkContext): Unit = {}
+    @(transient@getter)
+    var sparkSession: SparkSession = _
 
-  /**
-    * spark 停止后 程序结束前 调用
-    */
-  def beforeStop(sc: SparkContext): Unit = {}
+    /**
+      * 初始化，函数，可以设置 sparkConf
+      *
+      * @param sparkConf
+      */
+    def init(sparkConf: SparkConf): Unit = {}
 
-  /**
-    * 添加sparkListener
-    * 如使用此函数添加,则必须在 handle 函数之前调用此函数
-    *
-    * @param listener
-    * @deprecated 建议直接在配置文件中添加
-    */
-  @deprecated
-  def addSparkListeners(listener: String): Unit = sparkListeners += listener
+    /**
+      * spark 启动后 调用
+      */
+    def afterStarted(sc: SparkContext): Unit = {}
 
-  /**
-    * 处理函数
-    *
-    * @param sc
-    */
-  def handle(sc: SparkContext): Unit
+    /**
+      * spark 停止后 程序结束前 调用
+      */
+    def beforeStop(sc: SparkContext): Unit = {}
 
-  def creatingContext(): SparkContext = {
-    val sparkConf = new SparkConf()
+    /**
+      * 添加sparkListener
+      * 如使用此函数添加,则必须在 handle 函数之前调用此函数
+      *
+      * @param listener
+      * @deprecated 建议直接在配置文件中添加
+      */
+    @deprecated
+    def addSparkListeners(listener: String): Unit = sparkListeners += listener
 
-    sparkConf.set("spark.user.args", args.mkString("|"))
+    /**
+      * 处理函数
+      *
+      * @param sc
+      */
+    def handle(sc: SparkContext): Unit
 
-    // 约定传入此参数,则表示本地 Debug
-    if (sparkConf.contains("spark.properties.file")) {
-      sparkConf.setAll(Utils.getPropertiesFromFile(sparkConf.get("spark.properties.file")))
-      sparkConf.setAppName("LocalDebug").setMaster("local[*]")
-      sparkConf.set("spark.streaming.kafka.maxRatePerPartition", "10")
+    def creatingContext(): SparkContext = {
+        val sparkConf = new SparkConf()
+
+        sparkConf.set("spark.user.args", args.mkString("|"))
+        _debug = sparkConf.getBoolean("spark.fire.spark.debug", false)
+
+
+        // 约定传入此参数,则表示本地 Debug
+        if (sparkConf.contains("spark.properties.file")) {
+            sparkConf.setAll(Utils.getPropertiesFromFile(sparkConf.get("spark.properties.file")))
+            sparkConf.setAppName("LocalDebug").setMaster("local[*]")
+            sparkConf.set("spark.streaming.kafka.maxRatePerPartition", "10")
+        }
+
+        init(sparkConf)
+
+        val extraListeners = sparkListeners.mkString(",") + "," + sparkConf.get("spark.extraListeners", "")
+        if (extraListeners != "") sparkConf.set("spark.extraListeners", extraListeners)
+
+        sparkSession = SparkSession.builder().enableHiveSupport().config(sparkConf).getOrCreate()
+
+        val sc = sparkSession.sparkContext
+        handle(sc)
+        sc
     }
 
-    init(sparkConf)
+    def main(args: Array[String]): Unit = {
 
-    val extraListeners = sparkListeners.mkString(",") + "," + sparkConf.get("spark.extraListeners", "")
-    if (extraListeners != "") sparkConf.set("spark.extraListeners", extraListeners)
+        this._args = args
 
-    sparkSession = SparkSession.builder().enableHiveSupport().config(sparkConf).getOrCreate()
-
-    val sc = sparkSession.sparkContext
-    handle(sc)
-    sc
-  }
-
-  def main(args: Array[String]): Unit = {
-
-    this._args = args
-
-    val context = creatingContext()
-    afterStarted(context)
-    context.stop()
-    beforeStop(context)
-  }
+        val context = creatingContext()
+        afterStarted(context)
+        context.stop()
+        beforeStop(context)
+    }
 
 }
--- src/main/scala/org/fire/spark/streaming/core/FireStreaming.scala
+++ src/main/scala/org/fire/spark/streaming/core/FireStreaming.scala
@@ -1,8 +1,8 @@
 package org.fire.spark.streaming.core
 
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.{SparkConf, SparkContext}
 import org.fire.spark.streaming.core.kit.Utils
 
 import scala.annotation.meta.getter
@@ -17,144 +17,151 @@ import scala.collection.mutable.ArrayBuffer
 trait FireStreaming {
 
 
-  protected final def args: Array[String] = _args
+    protected final def args: Array[String] = _args
 
-  private final var _args: Array[String] = _
+    private final var _args: Array[String] = _
 
-  private val sparkListeners = new ArrayBuffer[String]()
+    // 添加debug 参数，便于统一控制
+    private final var _debug: Boolean = false
 
-  // checkpoint目录
-  private var checkpointPath: String = ""
+    def isDebug: Boolean = _debug
 
-  // 从checkpoint 中恢复失败，则重新创建
-  private var createOnError: Boolean = true
+    private val sparkListeners = new ArrayBuffer[String]()
 
-  @(transient@getter)
-  var sparkSession: SparkSession = _
+    // checkpoint目录
+    private var checkpointPath: String = ""
 
-  /**
-    * 初始化，函数，可以设置 sparkConf
-    *
-    * @param sparkConf
-    */
-  def init(sparkConf: SparkConf): Unit = {}
+    // 从checkpoint 中恢复失败，则重新创建
+    private var createOnError: Boolean = true
 
-  /**
-    * StreamingContext 运行之前执行
-    *
-    * @param ssc
-    */
-  def beforeStarted(ssc: StreamingContext): Unit = {}
+    @(transient@getter)
+    var sparkSession: SparkSession = _
 
-  /**
-    * StreamingContext 运行之后执行
-    */
-  def afterStarted(ssc: StreamingContext): Unit = {}
+    /**
+      * 初始化，函数，可以设置 sparkConf
+      *
+      * @param sparkConf
+      */
+    def init(sparkConf: SparkConf): Unit = {}
 
-  /**
-    * StreamingContext 停止后 程序停止前 执行
-    */
-  def beforeStop(ssc: StreamingContext): Unit = {}
+    /**
+      * StreamingContext 运行之前执行
+      *
+      * @param ssc
+      */
+    def beforeStarted(ssc: StreamingContext): Unit = {}
 
-  /**
-    * 添加sparkListener
-    * 如使用此函数添加,则必须在 handle 函数之前调用此函数
-    *
-    * @param listener
-    * @deprecated 建议直接在配置文件中添加
-    */
-  @deprecated
-  def addSparkListeners(listener: String): Unit = sparkListeners += listener
+    /**
+      * StreamingContext 运行之后执行
+      */
+    def afterStarted(ssc: StreamingContext): Unit = {}
 
-  /**
-    * 处理函数
-    *
-    * @param ssc
-    */
-  def handle(ssc: StreamingContext): Unit
+    /**
+      * StreamingContext 停止后 程序停止前 执行
+      */
+    def beforeStop(ssc: StreamingContext): Unit = {}
 
+    /**
+      * 添加sparkListener
+      * 如使用此函数添加,则必须在 handle 函数之前调用此函数
+      *
+      * @param listener
+      * @deprecated 建议直接在配置文件中添加
+      */
+    @deprecated
+    def addSparkListeners(listener: String): Unit = sparkListeners += listener
 
-  /**
-    * 创建 Context
-    *
-    * @return
-    */
-  def creatingContext(): StreamingContext = {
-
-    val sparkConf = new SparkConf()
-
-    sparkConf.set("spark.user.args", args.mkString("|"))
-
-    // 约定传入此参数,则表示本地 Debug
-    if (sparkConf.contains("spark.properties.file")) {
-      sparkConf.setAll(Utils.getPropertiesFromFile(sparkConf.get("spark.properties.file")))
-      sparkConf.setAppName("LocalDebug").setMaster("local[*]")
-      sparkConf.set("spark.streaming.kafka.maxRatePerPartition", "10")
-    }
-
-    init(sparkConf)
-
-    val extraListeners = sparkListeners.mkString(",") + "," + sparkConf.get("spark.extraListeners", "")
-    if (extraListeners != "") sparkConf.set("spark.extraListeners", extraListeners)
+    /**
+      * 处理函数
+      *
+      * @param ssc
+      */
+    def handle(ssc: StreamingContext): Unit
 
 
-    sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+    /**
+      * 创建 Context
+      *
+      * @return
+      */
+    def creatingContext(): StreamingContext = {
 
-    // 时间间隔
-    val slide = sparkConf.get("spark.batch.duration").toInt
-    val ssc = new StreamingContext(sparkSession.sparkContext, Seconds(slide))
-    handle(ssc)
-    ssc
-  }
+        val sparkConf = new SparkConf()
 
-
-  private def printUsageAndExit(): Unit = {
-
-    System.err.println(
-      """
-        |"Usage: FireStreaming [options]
-        |
-        | Options are:
-        |   --checkpointPath <checkpoint 目录设置>
-        |   --createOnError <从 checkpoint 恢复失败,是否重新创建 true|false>
-        |""".stripMargin)
-    System.exit(1)
-  }
+        sparkConf.set("spark.user.args", args.mkString("|"))
+        _debug = sparkConf.getBoolean("spark.fire.spark.debug", false)
 
 
-  def main(args: Array[String]): Unit = {
+        // 约定传入此参数,则表示本地 Debug
+        if (sparkConf.contains("spark.properties.file")) {
+            sparkConf.setAll(Utils.getPropertiesFromFile(sparkConf.get("spark.properties.file")))
+            sparkConf.setAppName("LocalDebug").setMaster("local[*]")
+            sparkConf.set("spark.streaming.kafka.maxRatePerPartition", "10")
+        }
 
-    this._args = args
+        init(sparkConf)
 
-    var argv = args.toList
-
-    while (argv.nonEmpty) {
-      argv match {
-        case ("--checkpointPath") :: value :: tail =>
-          checkpointPath = value
-          argv = tail
-        case ("--createOnError") :: value :: tail =>
-          createOnError = value.toBoolean
-          argv = tail
-        case Nil =>
-        case tail =>
-          System.err.println(s"Unrecognized options: ${tail.mkString(" ")}")
-          printUsageAndExit()
-      }
-    }
+        val extraListeners = sparkListeners.mkString(",") + "," + sparkConf.get("spark.extraListeners", "")
+        if (extraListeners != "") sparkConf.set("spark.extraListeners", extraListeners)
 
 
-    val context = checkpointPath match {
-      case "" => creatingContext()
-      case ck =>
-        val ssc = StreamingContext.getOrCreate(ck, creatingContext, createOnError = createOnError)
-        ssc.checkpoint(ck)
+        sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+
+        // 时间间隔
+        val slide = sparkConf.get("spark.batch.duration").toInt
+        val ssc = new StreamingContext(sparkSession.sparkContext, Seconds(slide))
+        handle(ssc)
         ssc
     }
-    beforeStarted(context)
-    context.start()
-    afterStarted(context)
-    context.awaitTermination()
-    beforeStop(context)
-  }
+
+
+    private def printUsageAndExit(): Unit = {
+
+        System.err.println(
+            """
+              |"Usage: FireStreaming [options]
+              |
+              | Options are:
+              |   --checkpointPath <checkpoint 目录设置>
+              |   --createOnError <从 checkpoint 恢复失败,是否重新创建 true|false>
+              |""".stripMargin)
+        System.exit(1)
+    }
+
+
+    def main(args: Array[String]): Unit = {
+
+        this._args = args
+
+        var argv = args.toList
+
+        while (argv.nonEmpty) {
+            argv match {
+                case ("--checkpointPath") :: value :: tail =>
+                    checkpointPath = value
+                    argv = tail
+                case ("--createOnError") :: value :: tail =>
+                    createOnError = value.toBoolean
+                    argv = tail
+                case Nil =>
+                case tail =>
+                    System.err.println(s"Unrecognized options: ${tail.mkString(" ")}")
+                    printUsageAndExit()
+            }
+        }
+
+
+        val context = checkpointPath match {
+            case "" => creatingContext()
+            case ck =>
+                val ssc = StreamingContext.getOrCreate(ck, creatingContext, createOnError = createOnError)
+                ssc.checkpoint(ck)
+                ssc
+        }
+        beforeStarted(context)
+        context.start()
+        afterStarted(context)
+        context.awaitTermination()
+        beforeStop(context)
+    }
 }
