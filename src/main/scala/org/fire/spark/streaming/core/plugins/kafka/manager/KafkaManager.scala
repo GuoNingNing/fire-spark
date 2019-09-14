81e6145934bed0d69b4715ac3e4c7fecc86db35a
shumeng.ren <shumeng.ren@meelive.cn>
2019-09-14 13:25:28 +0800

    [update] 1.修复设置group id，第一次不生效的bug

--- src/main/scala/org/fire/spark/streaming/core/plugins/kafka/manager/KafkaManager.scala
+++ src/main/scala/org/fire/spark/streaming/core/plugins/kafka/manager/KafkaManager.scala
@@ -94,12 +94,14 @@ private[kafka] class KafkaManager(val sparkConf: SparkConf) extends Logging with
                 ConsumerStrategies.Assign[K, V](consumerOffsets.keys, kafkaParams, consumerOffsets)
             )
         } else {
-            logInfo(s"read topics ==[$topics]== by  anonymity==")
+
+            // 匿名消费，生成临时消费组
+            val groupId = kafkaParams.getOrElse("group.id", s"anonymity-${System.currentTimeMillis()}")
+            logInfo(s"read topics ==[$topics]== by  group id ${groupId}==")
 
             KafkaUtils.createDirectStream[K, V](ssc,
                 LocationStrategies.PreferConsistent,
-                ConsumerStrategies.Subscribe[K, V](topics, kafkaParams ++ Map("group.id" -> s"anonymity-${System.currentTimeMillis()}"))
-//                ConsumerStrategies.Subscribe[K, V](topics, kafkaParams)
+                ConsumerStrategies.Subscribe[K, V](topics, kafkaParams ++ Map("group.id" -> groupId))
             )
         }
 
